0a81eee9230a69ed3a2c9f2b1eb3006a7f244119
carloterrado <iamterradocarlo@gmail.com>
2023-05-03 12:01:08 -0700

    recover column filter

--- resources/views/admin/inventory/inventoryTable.blade.php
+++ resources/views/admin/inventory/inventoryTable.blade.php
@@ -11,31 +11,44 @@
                         <th scope="col" class="text-center ">Thumbnail</th>
                         <th scope="col" class="text-center ">Name</th>
                         <th scope="col" class="text-center ">Color Identity</th>
-                        <th scope="col"  class="text-center">Type</th>
+                        <th scope="col" class="text-center">Type</th>
                         <th scope="col" class="text-center">Frame Effects</th>
                         <th scope="col" class="text-center">Finish</th>
                         <th scope="col" class="text-center">Rarity</th>
                         <th scope="col" class="col-1 text-center"><a href="#sort" data-toggle="modal"
-                                style="text-decoration: underline; color:black;">Quantity</a></th>
+                                style="text-decoration: none; color:black;">Quantity</a></th>
                         <th scope="col" class="text-center">TCG Mid</th>
                         <th scope="col" class="text-center">Total</th>
                         <th scope="col" class="text-center">Action</th>
                         @include('admin.inventory.action-popUp.sortQuantity')
                     </tr>
 
-                   
+
                 </thead>
 
                 <tbody>
                     @if (is_array($inventories) && count($inventories))
                         @foreach ($inventories as $item)
-                            @include('admin.inventory.sort-list.inventoryRow')
+                            {{-- Quantity Sorting --}}
+                            @if ($condition == '=' && $item['quantity'] == $value)
+                                @include('admin.inventory.sort-list.inventoryRow')
+                            @elseif ($condition == '<' && $item['quantity'] < $value)
+                                @include('admin.inventory.sort-list.inventoryRow')
+                            @elseif ($condition == '<=' && $item['quantity'] <= $value)
+                                @include('admin.inventory.sort-list.inventoryRow')
+                            @elseif ($condition == '>' && $item['quantity'] > $value)
+                                @include('admin.inventory.sort-list.inventoryRow')
+                            @elseif ($condition == '>=' && $item['quantity'] >= $value)
+                                @include('admin.inventory.sort-list.inventoryRow')
+                            @else
+                                @include('admin.inventory.sort-list.inventoryRow')
+                            @endif
                         @endforeach
                     @endif
 
                 </tbody>
                 <tfoot>
-                    <tr >
+                    <tr>
                         <td></td>
                         <td></td>
                         <td class="width-400">Name</td>
@@ -48,18 +61,17 @@
                         <td></td>
                         <td></td>
                         <td></td>
-                     
+
                     </tr>
 
                 </tfoot>
             </table>
 
         </div>
-        
     @else
         <br>
-        <div class="table-responsive-md">
-            {{-- <div class="row justify-content-center align-items-center">
+        <div class="container">
+            <div class="row justify-content-center align-items-center">
                 <div class="col-lg-8 text-center p-5">
                     <img src="https://icon-library.com/images/no-data-icon/no-data-icon-0.jpg" alt=""
                         style="widows: 100px; height: 100px;">
@@ -67,43 +79,7 @@
                     <br>
                     <h1>No data found. Please insert a CSV file.</h1>
                 </div>
-            </div> --}}
-            <table class="table table-sm table-hover bg-white" id="ojt_flow">
-                <thead class="bg-light table-group-divider table-divider-color">
-                    <tr class="tr-background">
-                        <th scope="col" class="text-center width-50">Selector</th>
-                        <th scope="col" class="text-center width-200">Thumbnail</th>
-                        <th scope="col" class="text-center width-200">Name</th>
-                        <th scope="col" class="text-center width-120">Color Identity</th>
-                        <th scope="col" class="text-center">Type</th>
-                        <th scope="col" class="text-center">Frame Effects</th>
-                        <th scope="col" class="text-center">Finish</th>
-                        <th scope="col" class="text-center">Rarity</th>
-                        <th scope="col" class="col-1 text-center"><a href="#sort" data-toggle="modal"
-                                style="text-decoration: underline; color:black;">Quantity</a></th>
-                        <th scope="col" class="text-center">TCG Mid</th>
-                        <th scope="col" class="text-center">Total</th>
-                        <th scope="col" class="text-center">Action</th>
-                        @include('admin.inventory.action-popUp.sortQuantity')
-                    </tr>
-                </thead>
-                <tbody>
-                    {{-- <tr>
-                        <td colspan="12" class="text-center">Upload Data</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr> --}}
-                </tbody>
-            </table>
+            </div>
         </div>
     @endif
 </div>
