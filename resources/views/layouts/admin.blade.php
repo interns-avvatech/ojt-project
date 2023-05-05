663cf85d05e08ce26a0f960989f78de9d565736a
chicky-krukie <erica.flores0307@gmail.com>
2023-05-05 12:56:24 -0700

    remove double header

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,33 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <div id="body-wrapper" class="body-container">
-
+<div id="body-wrapper" class="body-container" >
 
 
-        {{-- Sidebar --}}
-        <aside class="fixed-top vh-100" style="width: 250px; background-color: #2E3E4E">
-            <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: 220px;">
+    {{--Sidebar--}}
+    <aside class="fixed-top vh-100" style="width: 250px; background-color: #2E3E4E">
+        <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: 220px;">
 
-                @include('layouts.partials.sidebar')
+            @include('layouts.partials.sidebar')
 
-                {{-- <div class="slimScrollBar" style="background: rgb(204, 204, 204); width: 8px; position: absolute; top: 0px; opacity: 0.3; display: none; border-radius: 3px; z-index: 99; right: 0px; height: 752px;">
+            {{--<div class="slimScrollBar" style="background: rgb(204, 204, 204); width: 8px; position: absolute; top: 0px; opacity: 0.3; display: none; border-radius: 3px; z-index: 99; right: 0px; height: 752px;">
             </div>
-            <div class="slimScrollRail" style="width: 8px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 3px; background: transparent; opacity: 0.3; z-index: 90; right: 0px;"></div> --}}
-            </div>
-        </aside>
+            <div class="slimScrollRail" style="width: 8px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 3px; background: transparent; opacity: 0.3; z-index: 90; right: 0px;"></div>--}}
+        </div>
+    </aside>
 
 
-        {{-- page content --}}
-        <section class="main-container content-wrapper" style="margin-left: 250px">
-
-            {{-- header content --}}
-            <header class="main-nav clearfix sticky-top" style="width: calc(100% - 250px);">
-                @include('layouts.partials.headbar')
-            </header>
-
-
+    {{--page content--}}
+    <section class="main-container content-wrapper" style="margin-left: 250px">
 
         {{--header content--}}
         <header class="main-nav clearfix sticky-top w-100">
@@ -40,9 +31,7 @@
             @yield('admin-content')
         </div>
 
-            </div>
-
-            <div class="container-fluid page-content" style="margin-top: 80px; margin-bottom: 100px">
+        <div class="container-fluid page-content" style="margin-top: 80px; margin-bottom: 100px">
                 {{-- message code --}}
                 @if (session('error'))
                     <div class="alert alert-danger">
@@ -57,8 +46,14 @@
 
             </div>
 
-        </section>
+    </section>
 
 
-    </div>
+</div>
+
+
 @endsection
+
+
+
+
