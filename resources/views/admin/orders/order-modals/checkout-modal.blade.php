a9ad1f22e5cfc3a614ea710aee551a618e3234b4
carloterrado <iamterradocarlo@gmail.com>
2023-05-08 09:22:44 -0700

    add temporary address API

--- resources/views/admin/orders/order-modals/checkout-modal.blade.php
+++ resources/views/admin/orders/order-modals/checkout-modal.blade.php
@@ -64,34 +64,37 @@
                              <strong>Address:</strong>
                              <input required type="text" name="address" class="form-control" placeholder="Address">
                          </div>
-                         <div>
-                             <label for="region">Region:</label>
-                             <select name="region" id="region">
-                                 <option value="">Select Region</option>
-                                 @foreach ($location['regions'] as $region)
-                                     <option value="{{ $region['code'] }}">{{ $region['name'] }}</option>
-                                 @endforeach
-                             </select>
-                         </div>
-                         <div>
-                             <label for="province">Province:</label>
-                             <select name="province" id="province">
-                                 <option value="">Select Province</option>
-                             </select>
-                         </div>
-                         <div>
-                             <label for="municipality">Municipality:</label>
-                             <select name="municipality" id="municipality">
-                                 <option value="">Select Municipality</option>
-                             </select>
-                         </div>
-                         <div>
-                             <label for="barangay">Barangay:</label>
-                             <select name="barangay" id="barangay">
-                                 <option value="">Select Barangay</option>
-                             </select>
-                         </div>
 
+                         {{-- ADDRESS API --}}
+                         <div>
+                             <div>
+                                 <label for="region">Region:</label>
+                                 <select name="region" id="region">
+                                     <option>Select Region</option>
+                                     @foreach ($location['regions'] as $region)
+                                         <option value="{{ $region['code'] }}">{{ $region['name'] }}</option>
+                                     @endforeach
+                                 </select>
+                             </div>
+                             <div>
+                                 <label for="province">Province:</label>
+                                 <select name="province" id="province">
+                                     <option>Select Province</option>
+                                 </select>
+                             </div>
+                             <div>
+                                 <label for="municipality">Municipality:</label>
+                                 <select name="municipality" id="municipality">
+                                     <option>Select Municipality</option>
+                                 </select>
+                             </div>
+                             <div>
+                                 <label for="barangay">Barangay:</label>
+                                 <select name="barangay" id="barangay">
+                                     <option>Select Barangay</option>
+                                 </select>
+                             </div>
+                         </div>
                          <div class="mb-3">
                              <strong>Note:</strong>
                              <input type="text" name="note" value="" class="form-control"
@@ -123,5 +126,3 @@
          </div>
      </div>
  </div>
-
- 
